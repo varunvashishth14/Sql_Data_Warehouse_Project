cd05ee9005962dc6795e73e419e02e7ad19c1b94
varunvashishth14 <varunvashishth14@gmail.com>
2026-06-20 04:18:26 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -3,17 +3,17 @@
 DDL Script: Create Silver Tables
 ===============================================================================
 Script Purpose:
-    This script creates tables in the 'silver' schema, dropping existing tables 
+    This script creates tables in the 'Silver' schema, dropping existing tables 
     if they already exist.
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
 
-IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
+IF OBJECT_ID('Silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE Silver.crm_cust_info;
 GO
 
-CREATE TABLE silver.crm_cust_info (
+CREATE TABLE Silver.crm_cust_info (
     cst_id             INT,
     cst_key            NVARCHAR(50),
     cst_firstname      NVARCHAR(50),
@@ -25,11 +25,11 @@ CREATE TABLE silver.crm_cust_info (
 );
 GO
 
-IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_prd_info;
+IF OBJECT_ID('Silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE Silver.crm_prd_info;
 GO
 
-CREATE TABLE silver.crm_prd_info (
+CREATE TABLE Silver.crm_prd_info (
     prd_id          INT,
     cat_id          NVARCHAR(50),
     prd_key         NVARCHAR(50),
@@ -42,11 +42,11 @@ CREATE TABLE silver.crm_prd_info (
 );
 GO
 
-IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE silver.crm_sales_details;
+IF OBJECT_ID('Silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE Silver.crm_sales_details;
 GO
 
-CREATE TABLE silver.crm_sales_details (
+CREATE TABLE Silver.crm_sales_details (
     sls_ord_num     NVARCHAR(50),
     sls_prd_key     NVARCHAR(50),
     sls_cust_id     INT,
@@ -60,22 +60,22 @@ CREATE TABLE silver.crm_sales_details (
 );
 GO
 
-IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE silver.erp_loc_a101;
+IF OBJECT_ID('Silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE Silver.erp_loc_a101;
 GO
 
-CREATE TABLE silver.erp_loc_a101 (
+CREATE TABLE Silver.erp_loc_a101 (
     cid             NVARCHAR(50),
     cntry           NVARCHAR(50),
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
-IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE silver.erp_cust_az12;
+IF OBJECT_ID('Silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE Silver.erp_cust_az12;
 GO
 
-CREATE TABLE silver.erp_cust_az12 (
+CREATE TABLE Silver.erp_cust_az12 (
     cid             NVARCHAR(50),
     bdate           DATE,
     gen             NVARCHAR(50),
@@ -83,11 +83,11 @@ CREATE TABLE silver.erp_cust_az12 (
 );
 GO
 
-IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE silver.erp_px_cat_g1v2;
+IF OBJECT_ID('Silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE Silver.erp_px_cat_g1v2;
 GO
 
-CREATE TABLE silver.erp_px_cat_g1v2 (
+CREATE TABLE Silver.erp_px_cat_g1v2 (
     id              NVARCHAR(50),
     cat             NVARCHAR(50),
     subcat          NVARCHAR(50),
