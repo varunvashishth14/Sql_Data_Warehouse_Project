fe324da1f345d28bb2c55b999ec78615012a1eca
varunvashishth14 <varunvashishth14@gmail.com>
2026-06-20 07:26:54 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -4,7 +4,7 @@ Stored Procedure: Load Silver Layer (Bronze -> Silver)
 ===============================================================================
 Script Purpose:
     This stored procedure performs the ETL (Extract, Transform, Load) process to 
-    populate the 'silver' schema tables from the 'bronze' schema.
+    populate the 'Silver' schema tables from the 'bronze' schema.
 	Actions Performed:
 		- Truncates Silver tables.
 		- Inserts transformed and cleansed data from Bronze into Silver tables.
@@ -14,11 +14,11 @@ Parameters:
 	  This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    EXEC Silver.load_silver;
+    EXEC Silver.load_Silver;
 ===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE silver.load_silver AS
+CREATE OR ALTER PROCEDURE Silver.load_Silver AS
 BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
     BEGIN TRY
@@ -31,12 +31,12 @@ BEGIN
 		PRINT 'Loading CRM Tables';
 		PRINT '------------------------------------------------';
 
-		-- Loading silver.crm_cust_info
+		-- Loading Silver.crm_cust_info
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_cust_info';
-		TRUNCATE TABLE silver.crm_cust_info;
-		PRINT '>> Inserting Data Into: silver.crm_cust_info';
-		INSERT INTO silver.crm_cust_info (
+		PRINT '>> Truncating Table: Silver.crm_cust_info';
+		TRUNCATE TABLE Silver.crm_cust_info;
+		PRINT '>> Inserting Data Into: Silver.crm_cust_info';
+		INSERT INTO Silver.crm_cust_info (
 			cst_id, 
 			cst_key, 
 			cst_firstname, 
@@ -73,12 +73,12 @@ BEGIN
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
 
-		-- Loading silver.crm_prd_info
+		-- Loading Silver.crm_prd_info
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_prd_info';
-		TRUNCATE TABLE silver.crm_prd_info;
-		PRINT '>> Inserting Data Into: silver.crm_prd_info';
-		INSERT INTO silver.crm_prd_info (
+		PRINT '>> Truncating Table: Silver.crm_prd_info';
+		TRUNCATE TABLE Silver.crm_prd_info;
+		PRINT '>> Inserting Data Into: Silver.crm_prd_info';
+		INSERT INTO Silver.crm_prd_info (
 			prd_id,
 			cat_id,
 			prd_key,
@@ -106,17 +106,17 @@ BEGIN
 				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
 				AS DATE
 			) AS prd_end_dt -- Calculate end date as one day before the next start date
-		FROM bronze.crm_prd_info;
+		FROM Bronze.crm_prd_info;
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
 
         -- Loading crm_sales_details
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_sales_details';
-		TRUNCATE TABLE silver.crm_sales_details;
-		PRINT '>> Inserting Data Into: silver.crm_sales_details';
-		INSERT INTO silver.crm_sales_details (
+		PRINT '>> Truncating Table: Silver.crm_sales_details';
+		TRUNCATE TABLE Silver.crm_sales_details;
+		PRINT '>> Inserting Data Into: Silver.crm_sales_details';
+		INSERT INTO Silver.crm_sales_details (
 			sls_ord_num,
 			sls_prd_key,
 			sls_cust_id,
@@ -154,17 +154,17 @@ BEGIN
 					THEN sls_sales / NULLIF(sls_quantity, 0)
 				ELSE sls_price  -- Derive price if original value is invalid
 			END AS sls_price
-		FROM bronze.crm_sales_details;
+		FROM Bronze.crm_sales_details;
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
 
         -- Loading erp_cust_az12
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_cust_az12';
-		TRUNCATE TABLE silver.erp_cust_az12;
-		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
-		INSERT INTO silver.erp_cust_az12 (
+		PRINT '>> Truncating Table: Silver.erp_cust_az12';
+		TRUNCATE TABLE Silver.erp_cust_az12;
+		PRINT '>> Inserting Data Into: Silver.erp_cust_az12';
+		INSERT INTO Silver.erp_cust_az12 (
 			cid,
 			bdate,
 			gen
@@ -183,7 +183,7 @@ BEGIN
 				WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
 				ELSE 'n/a'
 			END AS gen -- Normalize gender values and handle unknown cases
-		FROM bronze.erp_cust_az12;
+		FROM Bronze.erp_cust_az12;
 	    SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
@@ -194,10 +194,10 @@ BEGIN
 
         -- Loading erp_loc_a101
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_loc_a101';
-		TRUNCATE TABLE silver.erp_loc_a101;
-		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
-		INSERT INTO silver.erp_loc_a101 (
+		PRINT '>> Truncating Table: Silver.erp_loc_a101';
+		TRUNCATE TABLE Silver.erp_loc_a101;
+		PRINT '>> Inserting Data Into: Silver.erp_loc_a101';
+		INSERT INTO Silver.erp_loc_a101 (
 			cid,
 			cntry
 		)
@@ -209,17 +209,17 @@ BEGIN
 				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
 				ELSE TRIM(cntry)
 			END AS cntry -- Normalize and Handle missing or blank country codes
-		FROM bronze.erp_loc_a101;
+		FROM Bronze.erp_loc_a101;
 	    SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
 		
 		-- Loading erp_px_cat_g1v2
 		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
-		TRUNCATE TABLE silver.erp_px_cat_g1v2;
-		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
-		INSERT INTO silver.erp_px_cat_g1v2 (
+		PRINT '>> Truncating Table: Silver.erp_px_cat_g1v2';
+		TRUNCATE TABLE Silver.erp_px_cat_g1v2;
+		PRINT '>> Inserting Data Into: Silver.erp_px_cat_g1v2';
+		INSERT INTO Silver.erp_px_cat_g1v2 (
 			id,
 			cat,
 			subcat,
